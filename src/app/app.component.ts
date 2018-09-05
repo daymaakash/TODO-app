3c0037cc2b1dfc8854b0f453bfd64a2dd88e4b40
daymaakash <daymaakash4@gmail.com>
2018-09-05 12:05:33 +0530

    my first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template:  ` <nav class='navbar navbar-expand navbar-light bg-light'>
-  <a class='navbar-brand'>{{pageTitle}}</a>
-  <ul class='nav nav-pills'>
-    <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/home']">Home</a></li>
-    <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/page']">my todo</a></li>
-  </ul>
-</nav>
-<div class='container'>
-<router-outlet></router-outlet>
-</div>
-`
+  templateUrl:'./app.component.html'
+
 })
 export class AppComponent {
   pageTitle = 'welcome to my todo';
